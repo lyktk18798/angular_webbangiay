53bf6bf09936df1a7b83fbcd7ac629aad5ccdc98
lyktk <lyktk@vnext.com.vn>
2020-05-05 22:32:50 +0700

    contact menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {ProductsComponent} from './components/products/products.component';
 import {DetailProductComponent} from './components/detail-product/detail-product.component';
 import {HomeComponent} from './components/home/home.component';
 import {CartComponent} from './components/cart/cart.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product-list/:groupId/:name/:color/:category', component: ProductsComponent },
   { path: 'product/:id', component: DetailProductComponent},
   { path: 'cart', component: CartComponent},
+  { path: 'contact', component: ContactComponent},
   { path: '**', pathMatch: 'full', redirectTo: '/' },
 ];
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../../service/product.service';
+import {Product} from '../../models/product';
 @Component({
   selector: 'app-products',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private apiServive: ProductService) {}
-  lstProductsSpecials = [
-    {
-      id: 1,
-      price: 5000,
-      name: 'Men\'sFootwear',
-      category: {
-        id: 1,
-      },
-      image: '408b9012d99721c97886.jpg'
-    }
-  ];
+  lstProductWoman: Product[] = [];
+  lstProductMan: Product[] = [];
+  lstProductKids: Product[] = [];
+  lstProductsSpecials: Product[] = [];
+
+  constructor(private apiService: ProductService) {}
+
   ngOnInit() {
+    this.getProductWoman();
+    this.getProductMan();
+    this.getProductKids();
   }
 
-  getProducts () {
+  getProductWoman () {
+    this.apiService.getProductByGroup(1)
+    .subscribe(rs => this.lstProductWoman = rs);
+  }
+
+  getProductMan () {
+    this.apiService.getProductByGroup(2)
+    .subscribe(rs => this.lstProductMan = rs);
+  }
+
+  getProductKids () {
+    this.apiService.getProductByGroup(3)
+    .subscribe(rs => this.lstProductKids = rs);
   }
 
 }
