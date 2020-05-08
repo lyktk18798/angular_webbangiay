2690df35733e641e8b9c3641bea805f59bb7cee9
lyktk <lyktk@vnext.com.vn>
2020-05-08 14:28:11 +0700

    Validate register page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,47 +1,53 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from '../../service/user.service';
-import {Role} from '../../models/role';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {User} from '../../models/user';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AlertService} from '../../service/alert.service';
+import {Customer} from '../../models/customer';
+import {AuthenticationService} from '../../service/authentication.service';
+import {MustMatch} from '../../helpers/must-match.validator';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: 'register.component.html'
+  templateUrl: 'register.component.html',
+  styleUrls: ['../login/login.component.css', './register.component.css']
 })
 export class RegisterComponent implements OnInit {
   saveForm: FormGroup;
-  user: User = new User();
-  constructor(private apiService: UserService,
+  user: Customer = new Customer();
+  submitted = false;
+  constructor(private apiService: AuthenticationService,
               private router: Router,
-              private alertService: AlertService) { }
+              private alertService: AlertService,
+              private formBuilder: FormBuilder) { }
   ngOnInit(): void {
-    this.saveForm = new FormGroup ({
-      email: new FormControl ('', [Validators.required, Validators.email]),
-      // fullname: new FormControl ('', Validators.required),
-      // phonenumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
+    this.saveForm = this.formBuilder.group({
+      phone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      comfirmPassword: ['', [Validators.required]]
+    }, {
+      validator: MustMatch('password', 'comfirmPassword')
     });
   }
 
   get email() { return this.saveForm.get('email'); }
-  // get fullname() { return this.saveForm.get('fullname'); }
-  // get phonenumber() { return this.saveForm.get('phonenumber'); }
-  get emailValid() {return this.email.invalid && (this.email.dirty || this.email.touched) && this.email.errors ; }
-  // get fullnameValid() {return this.fullname.invalid && (this.fullname.dirty || this.email.touched) && this.fullname.errors ; }
-  // get phoneValid() {return this.phonenumber.invalid && (this.phonenumber.dirty || this.phonenumber.touched) && this.phonenumber.errors ; }
+  get phone() { return this.saveForm.get('phone'); }
+  get password() { return this.saveForm.get('password'); }
+  get comfirmPassword() { return this.saveForm.get('comfirmPassword'); }
+  get emailValid() {return this.submitted || this.email.invalid && (this.email.dirty || this.email.touched) && this.email.errors ; }
+  get phoneValid() {return this.submitted || this.phone.invalid && (this.phone.dirty || this.phone.touched) && this.phone.errors ; }
+  get passwordValid() {return this.submitted || this.password.invalid && (this.password.dirty || this.password.touched) && this.password.errors ; }
+  get comfirmPasswordValid() {return this.submitted || this.comfirmPassword.invalid && (this.comfirmPassword.dirty || this.comfirmPassword.touched) && this.password.errors ; }
 
-  createAcc () {
+  register () {
+    this.submitted = true;
 // stop here if form is invalid
     if (this.saveForm.invalid) {
       return;
     }
     this.user.email = this.saveForm.value.email;
-    this.user.fullname = this.saveForm.value.fullname;
     this.user.phonenumber = this.saveForm.value.phonenumber;
-    this.user.role = new Role();
-    this.user.role.id = 3;
-    this.apiService.save(this.user)
+    this.apiService.register(this.user)
     .subscribe(rs => {
       console.log('done');
       this.router.navigate(['/login']);
